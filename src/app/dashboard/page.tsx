f6f01d090e3402ffd715ee5f226eb1f67cf601cc
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-10-21 20:48:48 +0100

    create init dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+interface DashboardProps {}
+
+async function Dashboard({}: DashboardProps): Promise<JSX.Element> {
+  return (
+    <>
+      <h1>Hier kommt das Dashboard</h1>
+    </>
+  );
+}
+
+export default Dashboard;
