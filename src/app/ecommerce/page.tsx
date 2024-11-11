e6020684bafbb00ab41b7c026c00c546019196d8
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-11-11 23:23:12 +0000

    add hero card and products map

--- src/app/ecommerce/page.tsx
+++ src/app/ecommerce/page.tsx
@@ -1,9 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import ProductCard from "./components/ProductCard";
+import prisma from "./lib/db/prisma";
+
 interface ECommerceHomeProps {}
 
-function ECommerceHome({}: ECommerceHomeProps): JSX.Element {
+async function ECommerceHome({}: ECommerceHomeProps): Promise<JSX.Element> {
+  const products = await prisma.product.findMany({
+    orderBy: { id: "desc" },
+  });
   return (
     <>
-      <h1>Startseite E-Commerce</h1>
+      <h1>All Products</h1>
+      <div className="hero min-h-screen rounded-xl bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <Image
+            src={products[0].imageUrl}
+            alt={products[0].name}
+            width={400}
+            height={800}
+            className="w-full max-w-sm rounded-lg shadow-2xl"
+            priority
+          />
+          <div>
+            <h1 className="text-5xl font-bold">{products[0].name}</h1>
+            <p className="py-6">{products[0].description}</p>
+            <Link
+              href={`/ecommerce/products/${products[0].id}`}
+              className="btn btn-primary"
+            >
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="my-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {products.slice(1).map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </>
   );
 }
