93629a1268f7367396cdef04d34d3a082c526cce
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-11-11 15:39:54 +0000

    add error page

---
+++ src/app/ecommerce/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+interface ErrorPageProps {}
+
+function ErrorPage({}: ErrorPageProps): JSX.Element {
+  return (
+    <>
+      <h1>Something went wrong. Please refresh the page.</h1>
+    </>
+  );
+}
+
+export default ErrorPage;
